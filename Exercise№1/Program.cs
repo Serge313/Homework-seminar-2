b6d639785441a60fa7fe1e23184c9f9d5983b996
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-14 23:50:59 +0300

    Exercise№1 completed

---
+++ Exercise№1/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = GetNumber("Enter your number ");
+int result = DelDigits(number);
+Console.WriteLine(result);
+
+int DelDigits(int a)
+{
+    return ((a % 100) / 10);
+}
+
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
