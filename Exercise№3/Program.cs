e2083d02b80bc68fd14fb800e418979a2baf06f8
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-15 00:21:49 +0300

    Exercise№3 completed

---
+++ Exercise№3/Program.cs
@@ -0,0 +1,22 @@
+﻿int number = GetNumber("Enter your number ");
+
+
+if (number >= 6 & number <= 7)
+{
+    Console.WriteLine("Yes");
+}
+else if (number > 0 & number <= 5)
+{
+    Console.WriteLine("No");
+}
+else
+{
+    Console.WriteLine("Error, choose another number");
+}
+
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
