9b7ebec76e266ece7a17b0a3262c5f39e89f2f56
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-15 18:41:24 +0300

    Exercise№2 completed

---
+++ Exercise№2/Program.cs
@@ -0,0 +1,19 @@
+﻿string number = GetNumber("Enter your number ");
+
+
+if (number.Length > 2)
+{
+    Console.WriteLine(number[2]);
+    Console.WriteLine(number.Length);
+}
+else if (number.Length <= 2)
+{
+    Console.WriteLine("There is no third digit");
+}
+
+
+string GetNumber(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+}
